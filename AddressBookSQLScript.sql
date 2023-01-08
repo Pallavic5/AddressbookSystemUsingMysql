df1a045d2911ed547cda1a173291746cc2b9b7e6
Pallavic5 <pchedge235@gmail.com>
2023-01-08 23:49:54 +0530

    Added: Logic of ability to create a addressbook table as its attributes.

--- AddressBookSQLScript.sql
+++ AddressBookSQLScript.sql
@@ -1,5 +1,19 @@
+
 /*UC 1 Ability to create a AddressBook Service Database - Use SQL client to create Database and Database records*/
 
 CREATE DATABASE `addressbookservice`;  /*create database*/
 USE addressbookservice;				  /*To use addressbookservice database*/
 SELECT database(); 					 /*To see current database*/
+
+/*UC 2 Ability to create a Address Book Table with first and last names, address, city, state, zip, phone number and email as its attributes*/
+
+CREATE TABLE `addressbook` (
+  `firstName` varchar(50) NOT NULL,			/*The NOT NULL constraint enforces a column to NOT accept NULL values.*/
+  `lastName` varchar(50) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `state` varchar(50) NOT NULL,
+  `zip` int NOT NULL,
+  `phoneNumber` double NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;

3748459b058f1653e5d355690d6edc3672740b9a
Pallavic5 <pchedge235@gmail.com>
2023-01-08 23:25:01 +0530

    Added: Logic of ability to create a addressbook service database and database records.

---
+++ AddressBookSQLScript.sql
@@ -0,0 +1,5 @@
+/*UC 1 Ability to create a AddressBook Service Database - Use SQL client to create Database and Database records*/
+
+CREATE DATABASE `addressbookservice`;  /*create database*/
+USE addressbookservice;				  /*To use addressbookservice database*/
+SELECT database(); 					 /*To see current database*/

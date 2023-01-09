b16346db1c7cfcd0f6ffe0412ae82cac5f4ea274
Pallavic5 <pchedge235@gmail.com>
2023-01-09 09:41:26 +0530

    Added: Logic of ability to get number of contact persons by count type .

--- AddressBookSQLScript.sql
+++ AddressBookSQLScript.sql
@@ -35,41 +35,45 @@ VALUES
  ('Shashwat', 'Patel','DattaApartment','Pune','Maharashtra', 341009, 9880565434,'sp@gmail.com');
  
  /*UC 4 Ability to edit existing contact person using their name.*/
-UPDATE `addressbookservice`.`addressbook` SET lastName='Agrawal' where firstname='Shashwat';
+UPDATE `addressbookservice`.`addressbook` SET lastName='Agrawal' WHERE firstname='Shashwat';
 SELECT * FROM AddressBook;
-UPDATE addressbook SET phoneNumber=7890065432 where firstname='Rahul';
+UPDATE addressbook SET phoneNumber=7890065432 WHERE firstname='Rahul';
 
 /*UC 5 Ability to delete a person using person's name*/
 DELETE FROM `addressbookservice`.`addressbook` WHERE lastName='Chedge';
 SELECT * FROM AddressBook;
 
 /*UC 6 Ability to retrieve person belonging to a city or state from the address book*/
-select * from AddressBook where city = 'Chennai' or State='TamilNadu';
+SELECT * FROM AddressBook WHERE city = 'Chennai' OR State='TamilNadu';
 
 /*UC 7 Ability to understand the size of address book by city and state - Here size indicates the count*/
-select count(firstname) from AddressBook where city = 'Mumbai';
-select count(firstname) from AddressBook where city = 'Nagpur';
-select count(firstname) from AddressBook where city = 'Pune';
-select count(firstname) from AddressBook where state='TamilNadu';
+SELECT count(firstname) FROM AddressBook WHERE city = 'Mumbai';
+SELECT count(firstname) FROM AddressBook WHERE city = 'Nagpur';
+SELECT count(firstname) FROM AddressBook WHERE city = 'Pune';
+SELECT count(firstname) FROM AddressBook WHERE state='TamilNadu';
 
 /*UC 8 Ability to retrieve entries sorted alphabetically by Person’s name for a given city */
- insert into AddressBook (firstname, lastname, address,city, state, zip,phoneNumber,email) values
+ INSERT INTO AddressBook (firstname, lastname, address,city, state, zip,phoneNumber,email) values
  ('Pallavi','Chedge','Lakewood','Nagpur','Maharashtra',600392,8907675123,'pc@gmail.com'),
  ('Ashwini','Kapse','BajajNagar','Surat','Gujarat',689765,7895644321,'ashwini@gmail.com');
- select * from AddressBook;
- select firstname,city from AddressBook order by city;
+ SELECT * FROM AddressBook;
+ SELECT firstname,city FROM	AddressBook ORDER BY city;
  
  /*UC 9 Ability to identify each Address Book with name and
 Type. - Here the type could Family, Friends, Profession, etc
 - Alter Address Book to add name and type*/
  
- alter table addressbook add addressbookName varchar(25) not null;
- alter table addressbook add contactType varchar(25) not null;
- desc addressbook;
- update AddressBook set addressbookname='one', contactType='Family' where lastname='Punewar';
- update AddressBook set addressbookname='two', contactType='Friends' where lastname='Kapse';
- update AddressBook set addressbookname='two', contactType='Profession' where lastname='Agrawal';
- select * from addressbook;
- select * from addressbook where contactType='Family';
- select * from AddressBook where contacttype='Profession';
- select * from AddressBook where contacttype='Friends';
+ ALTER TABLE addressbook ADD addressbookName varchar(25) NOT NULL;
+ ALTER TABLE addressbook ADD contactType varchar(25) NOT NULL;
+ DESC addressbook;
+ UPDATE AddressBook SET addressbookname='one', contactType='Family' WHERE lastname='Punewar';
+ UPDATE AddressBook SET addressbookname='two', contactType='Friends' WHERE lastname='Kapse';
+ UPDATE  AddressBook SET addressbookname='two', contactType='Profession' WHERE lastname='Agrawal';
+ SELECT * FROM addressbook;
+ SELECT * FROM addressbook WHERE contactType='Family';
+ SELECT * FROM AddressBook WHERE contacttype='Profession';
+ SELECT * FROM AddressBook WHERE contacttype='Friends';
+ 
+ /*UC 10 Ability to get number of contact persons i.e. count by type*/
+SELECT count(firstname) FROM AddressBook WHERE contactType='Profession';
+ 
